772503058e150d78e46594899a60c162e1ed60b1
ahishakiyeh <ahishakiyeh@yahoo.fr>
2018-03-08 11:22:49 +0100

    second commit of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -16,7 +16,21 @@ makeCacheMatrix <- function(x = matrix()) {
   }
 
 ## Write a short comment describing this function
-
+# cacheSolve computes the matrix inverse returned by the makeCacheMatrix function
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+## verification by calculate the inverse of a known matrix
+x = rbind(c(1, 3), c(2, 4))
+m = makeCacheMatrix(x)
+m$get()
+cacheSolve(m)
